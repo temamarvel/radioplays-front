ceb51e429f1683e63e9b3b1e12643ded4c0d0114
temamarvel <tema.denisoff@gmail.com>
2025-08-02 23:34:49 +0400

    play page can play audio

--- src/app/tracks/[id]/page.tsx
+++ src/app/tracks/[id]/page.tsx
@@ -18,7 +18,9 @@ export default async function TrackPage({ params }: TrackPageParams) {
     return (
         <main className="min-h-screen px-4 py-8 text-white bg-zinc-950">
             <h1 className="text-3xl font-bold mb-4">{track.name}</h1>
-            <p>Описание: {track.description}</p>
+            {/*Нет нормального дескрипшена. Сделать этот раздел из метаданых. Подключить синхронизацию с Diskog*/}
+            {/*Временно испотльзуем name*/}
+            <p>Описание: {track.name}</p>
 
             <div className="flex gap-2 mt-4 overflow-x-auto">
                 {track.cover_urls.map((url: string, idx: number) => (
@@ -31,10 +33,11 @@ export default async function TrackPage({ params }: TrackPageParams) {
                 ))}
             </div>
 
-            {/*<audio controls className="mt-6 w-full">*/}
-            {/*    <source src={track.audio_url} type="audio/mpeg" />*/}
-            {/*    Ваш браузер не поддерживает аудио.*/}
-            {/*</audio>*/}
+            <audio controls className="mt-6 w-full">
+                {/*Сейчас есть только один трек для каждой записи*/}
+                <source src={track.audio_urls[0]} type="audio/mpeg" />
+                Ваш браузер не поддерживает аудио.
+            </audio>
         </main>
     );
 }
